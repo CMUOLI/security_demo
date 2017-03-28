9ef3f1d2ab23bd2ddc977bd9de9f589febc5bc79
Anthony Corletti <anthcor@gmail.com>
2017-03-27 22:03:32 -0400

    complete build in container

--- django_micro_keycloak_oidc/django_micro/urls.py
+++ django_micro_keycloak_oidc/django_micro/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url, include
 from django.contrib import admin
 
 urlpatterns = [  
-    url(r'', include('demo.urls')),
+    url(r'', include('myapp.urls')),
     url(r'^admin/', admin.site.urls),
     url(r'openid/', include('djangooidc.urls')),
 ]
