d8cbb235f7028980d82658433434fb278167fbab
Anthony Corletti <anthcor@gmail.com>
2017-04-11 11:16:55 -0400

    small changes

--- django_micro_keycloak_oidc/myapp/views.py
+++ django_micro_keycloak_oidc/myapp/views.py
@@ -1,9 +1,11 @@
 from django.http import HttpResponse
-from django.contrib.auth.decorators import login_required
+from django.contrib.auth.decorators import login_required, permission_required
+from django.shortcuts import render
 
 def index(request):
     return HttpResponse('Please <a href="/secureme">Login</a>!')
 
 @login_required
+# @permission_required('myapp.can_login', raise_exception=True)
 def secureme(request):
-    return HttpResponse('You\'ve logged in! Now <a href="/openid/logout">Logout</a>')
+    return render(request, 'myapp/oidc_login.html')
